a4c5eaea8f8caf9dad3363be88829813d64c1b45
GeoffCraig <geoff.craig@hotmail.com>
2016-07-07 22:38:59 -0500

    Adding console.log to the endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 var appRouter = function(app){
     app.get("/status", function (req, res) {
+        console.log("someone hit ye ole status");
         return res.send({"text": "Hello Status!"})
     });
     app.get("/", function(req, res){
+        console.log("someone hit ye ole default");
         return res.send("Hello Meow!")
     });
 
