afe8b1fcee402e268ef41fa22d8006ff3b6db603
GeoffCraig <geoff.craig@hotmail.com>
2016-07-07 22:56:18 -0500

    Updating status response

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 var appRouter = function(app){
     app.get("/status", function (req, res) {
         console.log("someone hit ye ole status");
-        return res.send({"text": "Hello Status!"})
+        var sentData = req.query.command;
+        return res.send({"text": "Boro says " + sentData});
     });
     app.get("/", function(req, res){
         console.log("someone hit ye ole default");
-        return res.send("Hello Meow!")
+        return res.send("Hello Meow!");
     });
 
 }
