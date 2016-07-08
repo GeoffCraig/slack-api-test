f6f2e9513bc66efcb14943ae9ff6b2fa620bd61d
GeoffCraig <geoff.craig@hotmail.com>
2016-07-07 22:35:32 -0500

    Adding default route so heroku dont hate me

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
 var appRouter = function(app){
     app.get("/status", function (req, res) {
         return res.send({"text": "Hello Status!"})
-    })
+    });
+    app.get("/", function(req, res){
+        return res.send("Hello Meow!")
+    });
 
 }
 
